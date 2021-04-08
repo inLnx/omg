e26705cb8da814ddbc38404817782126436a6783
Glitch (hello-webpage) <none>
2021-04-08 15:46:45 +0000

    🕶💅 Checkpoint ./script.js:963655/4235 ./index.html:963655/29 ./style.css:963655/216

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-import limitConcur from 'https://unpkg.com/limit-concur@0.0.1/src/index.js';
+import limitConcur from "https://unpkg.com/limit-concur@0.0.1/src/index.js";
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
-    await navigator.serviceWorker.register('sw.js');
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    await navigator.serviceWorker.register("sw.js");
   });
 }
 
+const removeNonCharacters = text => {
+  return text.replace(/\W/g, "-").replace(/-+/g, "-");
+};
+
 (async () => {
-  const categories = await fetch('https://api.chucknorris.io/jokes/categories').then(response => response.json())
+  const categories = await fetch(
+    "https://api.chucknorris.io/jokes/categories"
+  ).then(response => response.json());
 
   async function getChuckNorrisJoke(category) {
-    const { value } = await fetch(`https://api.chucknorris.io/jokes/random?${category}`).then(response => response.json())      
-    return value
+    const { value } = await fetch(
+      `https://api.chucknorris.io/jokes/random?${category}`
+    ).then(response => response.json());
+    return value;
   }
 
-  const limitedGetChuckNorrisJoke = limitConcur(4, getChuckNorrisJoke)
-  const jokes = await Promise.all(categories.map(limitedGetChuckNorrisJoke))
-  
-  const ul = document.querySelector('ul')
-  const html = '';
-  jokes.forEach((joke) => {
-    html += `<li data-search="${joke.}">`
-  })
+  const limitedGetChuckNorrisJoke = limitConcur(4, getChuckNorrisJoke);
+  const jokes = await Promise.all(categories.map(limitedGetChuckNorrisJoke));
+
+  let html = "";
+  jokes.forEach(joke => {
+    html += `<li data-search="${removeNonCharacters(joke)}">${joke}</li>`;
+  });
+  ul.innerHTML = html;
 })();
+
+input.addEventListener("input", e => {
+  ul.querySelectorAll("li").forEach(li => li.classList.remove("highlight"));
+  if (!input.value || !ul.innerHTML) {
+    return;
+  }
+  ul.querySelectorAll(
+    `[data-search*="${removeNonCharacters(input.value)}"]`
+  ).forEach(li => {
+    li.classList.add("highlight");
+  });
+});
