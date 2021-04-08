d1b21bfca50fc67a2b8dfb8be3a6290cc5ac585d
Glitch (hello-webpage) <none>
2021-04-08 15:34:57 +0000

    🌊🏆 Checkpoint ./manifest.webmanifest:963655/86 ./script.js:963655/3320 ./sw.js:963655/1175 ./index.html:963655/470

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+import limitConcur from 'https://unpkg.com/limit-concur@0.0.1/src/index.js';
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    await navigator.serviceWorker.register('sw.js');
+  });
+}
+
+(async () => {
+  const categories = await fetch('https://api.chucknorris.io/jokes/categories').then(response => response.json())
+
+  async function getChuckNorrisJoke(category) {
+    const { value } = await fetch(`https://api.chucknorris.io/jokes/random?${category}`).then(response => response.json())      
+    return value
+  }
+
+  const limitedGetChuckNorrisJoke = limitConcur(4, getChuckNorrisJoke)
+  const jokes = await Promise.all(categories.map(limitedGetChuckNorrisJoke))
+  
+  const ul = document.querySelector('ul')
+  const html = '';
+  jokes.forEach((joke) => {
+    html += `<li data-search="${joke.}">`
+  })
+})();
---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'chuck-norris-cache';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './script.js',
+  './style.css',
+  './manifest.webmanifest',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    Promise.all(
+      keys.map(async (key) => {
+        if (key !== CACHE_NAME) {
+          await caches.delete(key)
+        }
+      })
+    )
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    return fetch(e.request).catch(() => caches.match(e.request))
+  })())
+})
